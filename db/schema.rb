b1a97937978931fd58861e9e3fb113aa43e5de55
Jakub Janco <kubco2@gmail.com>
2015-12-06 21:44:09 +0100

    fix avatars in posts

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,80 @@ ActiveRecord::Schema.define(version: 20151206191730) do
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], name: "index_roles_on_name"
 
+  create_table "so_posts", force: :cascade do |t|
+    t.string   "author"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "so_posts", ["user_id"], name: "index_so_posts_on_user_id"
+
+  create_table "so_posts_tags", id: false, force: :cascade do |t|
+    t.integer "so_post_id", null: false
+    t.integer "tag_id",     null: false
+  end
+
+  create_table "so_roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "so_roles", ["name", "resource_type", "resource_id"], name: "index_so_roles_on_name_and_resource_type_and_resource_id"
+  add_index "so_roles", ["name"], name: "index_so_roles_on_name"
+
+  create_table "so_schema_migrations", id: false, force: :cascade do |t|
+    t.string "version", null: false
+  end
+
+  add_index "so_schema_migrations", ["version"], name: "so_unique_schema_migrations", unique: true
+
+  create_table "so_tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "so_tags", ["name"], name: "index_so_tags_on_name", unique: true
+
+  create_table "so_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  add_index "so_users", ["email"], name: "index_so_users_on_email", unique: true
+  add_index "so_users", ["reset_password_token"], name: "index_so_users_on_reset_password_token", unique: true
+
+  create_table "so_users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "so_users_roles", ["user_id", "role_id"], name: "index_so_users_roles_on_user_id_and_role_id"
+
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -59,17 +133,20 @@ ActiveRecord::Schema.define(version: 20151206191730) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
