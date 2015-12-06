222336ac2fd699a90539857ee04f6abd2349ccd9
Jakub Janco <kubco2@gmail.com>
2015-12-06 16:13:13 +0100

    missing files

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  rolify
+  validates :email, presence: true
+  validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+end
